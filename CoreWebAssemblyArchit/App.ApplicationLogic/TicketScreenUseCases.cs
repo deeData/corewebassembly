c3014e3617afa25d9110078c04979bfac1c43f89
deedata <deidra108@gmail.com>
2021-10-11 12:26:40 -0600

    add Ticket use case in biz logic, and tickets razor component in web app

---
+++ CoreWebAssemblyArchit/App.ApplicationLogic/TicketScreenUseCases.cs
@@ -0,0 +1,32 @@
+﻿using App.Repository.webApi;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.ApplicationLogic
+{
+    public class TicketScreenUseCases : ITicketScreenUseCases
+    {
+        private readonly IProjectRepository projectRepository;
+
+        //using project endpoints
+        public TicketScreenUseCases(IProjectRepository projectRepository)
+        {
+            this.projectRepository = projectRepository;
+        }
+
+        public async Task<IEnumerable<Ticket>> ViewTickets(int projectId)
+        {
+            return await projectRepository.GetProjectTicketsAsync(projectId);
+        }
+
+
+
+
+
+
+    }
+}
